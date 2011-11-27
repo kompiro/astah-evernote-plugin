511f7099d8995da7cee6739811043834210a5680
Hiroki Kondo <kompiro@gmail.com>
2011-11-27 22:36:39 +0900

    improved cancel logic

--- src/main/java/com/change_vision/astah/evernote/dialog/LoginDialog.java
+++ src/main/java/com/change_vision/astah/evernote/dialog/LoginDialog.java
@@ -13,17 +13,23 @@ import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
 public class LoginDialog extends JDialog implements ActionListener {
-	
+
+	private final class CancelActionListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			setVisible(false);
+		}
+	}
+
 	private static final String ACTION_LOGIN = "login"; //$NON-NLS-1$
 	private static final String ACTION_CANCEL = "cancel"; //$NON-NLS-1$
 
 	private String username = ""; //$NON-NLS-1$
 	private String password = ""; //$NON-NLS-1$
-	
+
 	private JTextField userField = new JTextField(15);
 	private JPasswordField passField = new JPasswordField(15);
 
-	boolean init = false;
 	private boolean canceled = true;
 
 	public LoginDialog(Window window) {
@@ -31,36 +37,35 @@ public class LoginDialog extends JDialog implements ActionListener {
 		setTitle(Messages.getString("LoginDialog.title")); //$NON-NLS-1$
 		setModal(true);
 		getContentPane().setLayout(new GridLayout(3, 2));
-		getContentPane().add(new JLabel(Messages.getString("LoginDialog.username_label"))); //$NON-NLS-1$
+		getContentPane().add(
+				new JLabel(Messages.getString("LoginDialog.username_label"))); //$NON-NLS-1$
 		getContentPane().add(userField);
-		getContentPane().add(new JLabel(Messages.getString("LoginDialog.password_label"))); //$NON-NLS-1$
+		userField.addActionListener(this);
+		getContentPane().add(
+				new JLabel(Messages.getString("LoginDialog.password_label"))); //$NON-NLS-1$
 		getContentPane().add(passField);
-		JButton login = new JButton(Messages.getString("LoginDialog.login_label")); //$NON-NLS-1$
+		passField.addActionListener(this);
+		JButton login = new JButton(
+				Messages.getString("LoginDialog.login_label")); //$NON-NLS-1$
 		login.setActionCommand(ACTION_LOGIN);
 		getContentPane().add(login);
 		login.addActionListener(this);
-		JButton cancel = new JButton(Messages.getString("LoginDialog.cancel_label")); //$NON-NLS-1$
+		JButton cancel = new JButton(
+				Messages.getString("LoginDialog.cancel_label")); //$NON-NLS-1$
 		cancel.setActionCommand(ACTION_CANCEL);
 		getContentPane().add(cancel);
-		cancel.addActionListener(this);
+		cancel.addActionListener(new CancelActionListener());
 		pack();
 		setLocationRelativeTo(window);
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		String actionCommand = e.getActionCommand();
-		if(actionCommand== null) return;
-		if(actionCommand.equals(ACTION_LOGIN)){
-			canceled = false;
-			username = userField.getText();
-			password = new String(passField.getPassword());
-		}else if(actionCommand.equals(ACTION_CANCEL)){
-			canceled = true;
-		}
-		init = true;
+		canceled = false;
+		username = userField.getText();
+		password = new String(passField.getPassword());
 		setVisible(false);
 	}
-	
+
 	public boolean isCanceled() {
 		return canceled;
 	}
@@ -68,9 +73,14 @@ public class LoginDialog extends JDialog implements ActionListener {
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public String getPassword() {
 		return password;
+	}
+
+	public static void main(String[] args) {
+		LoginDialog dialog = new LoginDialog(null);
+		dialog.setVisible(true);
 	}
 
 }
